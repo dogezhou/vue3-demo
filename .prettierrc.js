5a38e359004dd8b6471c63934cdbf3a3a375cf33
dogezhou <doge.zhou@outlook.com>
2020-10-10 17:51:06 +0800

    chore: fix line breaker setting of linter

--- .prettierrc.js
+++ .prettierrc.js
@@ -16,5 +16,7 @@ module.exports = {
     // 箭头函数参数括号 默认avoid 可选 avoid| always
     // avoid 能省略括号的时候就省略 例如x => x
     // always 总是有括号
-    arrowParens: 'always'
+    arrowParens: 'always',
+    // 不管换行符
+    endOfLine: "auto"
 }
